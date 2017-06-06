8bd20853586392e02fb5c8350ff1c8f83113bab0
Erika Vidal <diann852@gmail.com>
2017-06-06 14:16:21 -0500

    primer commit

---
+++ js/components/header.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Header = (servicioGrid) => {
+  const header = $("<header></header>");
+  const div = $('<div class="row valign-wrapper"></div>');
+  const title = $("<span class='col s12 left-align'>Gas Finder</span>");
+  const icon = $('<i class="fa fa-chevron-left col s1 left-align hide" aria-hidden="true"></i>');
+  const divInput = $('<div class="buscar"></div>');
+  const input = $('<input class="col s12" type="text" placeholder="Ingresa tu distrito a buscar">');
+  const iconSearch = $('<i class="fa fa-search"></i>');
+
+  div.append(icon);
+  div.append(title);
+  input.append(iconSearch);
+  divInput.append(input);
+  header.append(div);
+  header.append(divInput);
+
+  input.on('keyup',(e)=>{
+    servicioGrid(input.val());
+    })
+
+  return header;
+}
